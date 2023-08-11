750a6fca3d9cb9ac83c905967efac2efca9828ed
manibharathi0479 <manibharathir1606@gmail.com>
2023-08-11 17:24:30 +0530

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
+
+import { TemplateformComponent } from './templateform/templateform.component';
+import { ReactiveformsComponent } from './reactiveforms/reactiveforms.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateformComponent,
+    ReactiveformsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
